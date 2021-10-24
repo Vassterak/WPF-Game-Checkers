984a7c3d2e1bb429df5370093364a78b4781a4d3
Vassterak <vaclavhorcicka@outlook.cz>
2021-10-24 16:19:57 +0200

    Clean up #1 (deviding into classes)

--- Checkers.cs
+++ Checkers.cs
@@ -5,8 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace WPF_Game_Checkers
 {
@@ -14,8 +19,12 @@ namespace WPF_Game_Checkers
     {
         //default values
         private int xSize = 8, ySize = 8, numOfStoneRows = 2;
+        public bool[,] validPosition;
         public FrameworkElement playerStone;
         public FrameworkElement lastPlayerStonePosition;
+        public Ellipse[,] playersLocation;
+        public bool freeMovement = false, whiteMove = false;
+        public int attempsForMove = 0, moves = 0;
 
         public int XSize
         {
@@ -32,6 +41,186 @@ namespace WPF_Game_Checkers
         {
             get { return numOfStoneRows; }
             set { numOfStoneRows = value; }
+        }
+
+        public void CreateGameGrid(Grid gameGrid) //Crete grid for the game content.
+        {
+            //Initialize arrays
+            validPosition = new bool[XSize,YSize];
+            playersLocation = new Ellipse[XSize, YSize];
+
+            for (int y = 0; y < YSize; y++)
+            {
+                RowDefinition row = new RowDefinition();
+                gameGrid.RowDefinitions.Add(row);
+            }
+
+            for (int x = 0; x < XSize; x++)
+            {
+                ColumnDefinition colum = new ColumnDefinition();
+                gameGrid.ColumnDefinitions.Add(colum);
+            }
+        }
+
+        public void RenderCheckeredBackground(Grid gameGrid)
+        {
+            bool oddRectangle = true;
+
+            for (int y = 0; y < YSize; y++)
+            {
+                for (int x = 0; x < XSize; x++)
+                {
+                    validPosition[y, x] = !oddRectangle;
+
+                    Rectangle rectangle = new Rectangle { Fill = oddRectangle ? Brushes.LightGray : Brushes.SaddleBrown };
+                    rectangle.SetValue(Grid.RowProperty, y);
+                    rectangle.SetValue(Grid.ColumnProperty, x);
+                    gameGrid.Children.Add(rectangle);
+
+                    oddRectangle = OddChecker(oddRectangle, x); //Creating the checkboard pattern
+                }
+            }
+        }
+
+        public void RenderStones(int numOfStonesRows, Grid gameGrid, Style stoneStyle)
+        {
+            bool blackPlane = false, player2render = false;
+
+            for (int y = 0; y < YSize; y++)
+            {
+                for (int x = 0; x < XSize; x++)
+                {
+                    //Render stones only on brown spots
+                    if (blackPlane)
+                    {
+                        if (y < numOfStonesRows) //render for player 1 (at top)
+                        {
+                            Ellipse newPlayerStone = new Ellipse { Fill = Brushes.White, Margin = new Thickness(10) };
+                            newPlayerStone.SetValue(Grid.RowProperty, y);
+                            newPlayerStone.SetValue(Grid.ColumnProperty, x);
+                            newPlayerStone.Name = $"white_{x}_{y}";
+                            newPlayerStone.Style = stoneStyle; //Apply style with event handler
+
+                            gameGrid.Children.Add(newPlayerStone); //Adding player's stone to be rendered in gameGrid
+                            playersLocation[y, x] = newPlayerStone;
+                        }
+
+                        else //render for player 2 (at bottom)
+                        {
+                            if (!player2render)
+                            {
+                                y = YSize - numOfStonesRows;
+                                player2render = true;
+                            }
+
+                            Ellipse newPlayerStone = new Ellipse { Fill = Brushes.Black, Margin = new Thickness(10) };
+                            newPlayerStone.SetValue(Grid.RowProperty, y);
+                            newPlayerStone.SetValue(Grid.ColumnProperty, x);
+                            newPlayerStone.Name = $"black_{x}_{y}";
+                            newPlayerStone.Style = stoneStyle; //Apply style with event handler
+
+                            gameGrid.Children.Add(newPlayerStone); //Adding player's stone to be rendered in gameGrid
+                            playersLocation[y, x] = newPlayerStone;
+                        }
+                    }
+                    else
+                        playersLocation[y, x] = null;
+
+                    blackPlane = OddChecker(blackPlane, x); //Set if the checkboard plane is black
+                }
+            }
+        }
+
+        public bool OddChecker(bool oddRectangle, int x) //Return true only when last plate was false
+        {
+            oddRectangle = !oddRectangle;
+            if (x == XSize - 1 && XSize % 2 == 0) //only true when x(rows) are even
+                oddRectangle = !oddRectangle;
+
+            return oddRectangle;
+        }
+
+        public bool MoveOneWayOnly(FrameworkElement lastPostion, FrameworkElement currentPosition, Grid gameGrid)
+        {
+            int yLast = 0, yNow = 0;
+
+            yLast = Grid.GetRow(lastPostion);
+            yNow = Grid.GetRow(currentPosition);
+
+            if (lastPostion.Name.Substring(0, 5) == "white")
+            {
+                if ((yNow - yLast) == 1)
+                    return true;
+
+                else if ((yNow - yLast) == 2)
+                    return CheckKill(lastPostion, currentPosition, gameGrid);
+            }
+
+            else
+            {
+                if ((yNow - yLast) == -1)
+                    return true;
+
+                else if ((yNow - yLast) == -2)
+                    return CheckKill(lastPostion, currentPosition, gameGrid);
+            }
+
+            return false;
+        }
+
+        public bool CheckKill(FrameworkElement lastPosition, FrameworkElement currentPosition, Grid gameGrid)
+        {
+            int xVictim = 0, yVictim = 0;
+
+            yVictim = (Grid.GetRow(lastPosition) + Grid.GetRow(currentPosition)) / 2;
+            xVictim = (Grid.GetColumn(lastPosition) + Grid.GetColumn(currentPosition)) / 2;
+
+            if (playersLocation[yVictim, xVictim] != null && !freeMovement)
+            {
+                var victim = gameGrid.Children.Cast<FrameworkElement>().Last(value => Grid.GetRow(value) == yVictim && Grid.GetColumn(value) == xVictim); //Find UIElement by row/culumn values. ".Last" because Ellipses were created after rectangles(checkerboard)
+
+                if (victim.Name.Substring(0, 5) == lastPosition.Name.Substring(0, 5)) //Check if player's stones have different colors. if they are same return false
+                    return false;
+
+                gameGrid.Children.Remove(victim); //remove adversary
+
+                playersLocation[yVictim, xVictim] = null; //freeup space after elimination in gamesGrid
+                return true;
+            }
+            return false;
+        }
+
+        public Tuple<bool, bool> CheckAdversaryAlive()
+        {
+            bool white = false, black = false;
+
+            foreach (var item in playersLocation)
+            {
+                if (item != null)
+                {
+                    switch (item.Name.Substring(0, 5))
+                    {
+                        case "white":
+                            white = true;
+                            break;
+
+                        case "black":
+                            black = true;
+                            break;
+                    }
+                }
+
+                if (black && white)
+                    return Tuple.Create(true, false);
+            }
+            return Tuple.Create(false, white);
+        }
+
+        public void ResetGame(Grid gameGrid)
+        {
+            gameGrid.Children.Clear();
+            gameGrid.RowDefinitions.Clear();
+            gameGrid.ColumnDefinitions.Clear();
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,71 +17,12 @@ namespace WPF_Game_Checkers
 {
     public partial class MainWindow : Window
     {
-        private Random rnd = new Random();
         private Checkers damaGame = new Checkers();
-        private bool[,] validPosition;
-        private Ellipse[,] playersLocation;
-        private bool freeMovement = false, whiteMove = false;
-        private int attempsForMove = 0, moves = 0;
+        private Random rnd = new Random();
 
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void CreateGameGrid() //Crete grid for the game content.
-        {
-            //Initialize arrays
-            validPosition = new bool[damaGame.XSize, damaGame.YSize]; 
-            playersLocation = new Ellipse[damaGame.XSize, damaGame.YSize];
-
-            for (int y = 0; y < damaGame.YSize; y++)
-            {
-                RowDefinition row = new RowDefinition();
-                gameGrid.RowDefinitions.Add(row);
-            }
-
-            for (int x = 0; x < damaGame.XSize; x++)
-            {
-                ColumnDefinition colum = new ColumnDefinition();
-                gameGrid.ColumnDefinitions.Add(colum);
-            }
-
-            if (freeMovementCheckBox.IsChecked == true)
-            {
-                freeMovement = true;
-                MessageBox.Show("Volný pohyb je povolen, nyní nemůžeš vyřazovat proti-hráče hráče."); //Free movement is disabled. Now you are unable to eliminate the adversary.
-            }
-            else
-                freeMovement = false;
-
-            if (rnd.Next(1, 6) > 3) //Random selection figures colors for opening move
-            {
-                whiteMove = true;
-                player1Label.Background = Brushes.Green;
-            }
-            else
-                player2Label.Background = Brushes.Green;
-        }
-
-        private void RenderCheckeredBackground()
-        {
-            bool oddRectangle = true;
-
-            for (int y = 0; y < damaGame.YSize; y++)
-            {
-                for (int x = 0; x < damaGame.XSize; x++)
-                {
-                    validPosition[y, x] = !oddRectangle;
-
-                    Rectangle rectangle = new Rectangle { Fill = oddRectangle ? Brushes.LightGray : Brushes.SaddleBrown };
-                    rectangle.SetValue(Grid.RowProperty, y);
-                    rectangle.SetValue(Grid.ColumnProperty, x);
-                    gameGrid.Children.Add(rectangle);
-
-                    oddRectangle = OddChecker(oddRectangle, x); //Creating the checkboard pattern
-                }
-            }
         }
 
         private void RenderDebugText()
@@ -106,93 +47,45 @@ namespace WPF_Game_Checkers
             }
         }
 
-        private void ResetGame()
-        {
-            gameGrid.Children.Clear();
-            gameGrid.RowDefinitions.Clear();
-            gameGrid.ColumnDefinitions.Clear();
-            player1Label.Background = null;
-            player2Label.Background = null;
-        }
-
-        private void RenderStones(int numOfStonesRows)
-        {
-            bool blackPlane = false, player2render = false;
-
-            for (int y = 0; y < damaGame.YSize; y++)
-            {
-                for (int x = 0; x < damaGame.XSize; x++)
-                {
-                    //Render stones only on brown spots
-                    if (blackPlane)
-                    {
-                        if (y < numOfStonesRows) //render for player 1 (at top)
-                        {
-                            Ellipse newPlayerStone = new Ellipse { Fill = Brushes.White, Margin = new Thickness(10) };
-                            newPlayerStone.SetValue(Grid.RowProperty, y);
-                            newPlayerStone.SetValue(Grid.ColumnProperty, x);
-                            newPlayerStone.Name = $"white_{x}_{y}";
-                            newPlayerStone.Style = (Style)FindResource("playerStyle"); //Apply style with event handler
-
-                            gameGrid.Children.Add(newPlayerStone); //Adding player's stone to be rendered in gameGrid
-                            playersLocation[y, x] = newPlayerStone;
-                        }
-
-                        else //render for player 2 (at bottom)
-                        {
-                            if (!player2render)
-                            {
-                                y = damaGame.YSize - numOfStonesRows;
-                                player2render = true;
-                            }
-
-                            Ellipse newPlayerStone = new Ellipse { Fill = Brushes.Black, Margin = new Thickness(10) };
-                            newPlayerStone.SetValue(Grid.RowProperty, y);
-                            newPlayerStone.SetValue(Grid.ColumnProperty, x);
-                            newPlayerStone.Name = $"black_{x}_{y}";
-                            newPlayerStone.Style = (Style)FindResource("playerStyle"); //Apply style with event handler
-
-                            gameGrid.Children.Add(newPlayerStone); //Adding player's stone to be rendered in gameGrid
-                            playersLocation[y, x] = newPlayerStone;
-                        }
-                    }
-                    else
-                        playersLocation[y, x] = null;
-
-                    blackPlane = OddChecker(blackPlane, x); //Set if the checkboard plane is black
-                }
-            }
-        }
 
         private void ButtonNewGame_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 //Clear everything
-                ResetGame();
+                damaGame.ResetGame(gameGrid);
+                player1Label.Background = null;
+                player2Label.Background = null;
 
                 damaGame.XSize = int.Parse(textBoxWidth.Text);
                 damaGame.YSize = int.Parse(textBoxHeight.Text);
-                damaGame.NumOfStoneRows = int.Parse(textBoxNumStoneRows.Text);
 
-                CreateGameGrid();
-                RenderCheckeredBackground();
-                RenderStones(damaGame.NumOfStoneRows);
+                damaGame.CreateGameGrid(gameGrid);
+
+                if (freeMovementCheckBox.IsChecked == true)
+                {
+                    damaGame.freeMovement = true;
+                    MessageBox.Show("Volný pohyb je povolen, nyní nemůžeš vyřazovat proti-hráče hráče."); //Free movement is disabled. Now you are unable to eliminate the adversary.
+                }
+                else
+                    damaGame.freeMovement = false;
+
+                if (rnd.Next(1, 6) > 3) //Random selection figures colors for opening move
+                {
+                    damaGame.whiteMove = true;
+                    player1Label.Background = Brushes.Green;
+                }
+                else
+                    player2Label.Background = Brushes.Green;
+
+                damaGame.RenderCheckeredBackground(gameGrid);
+                damaGame.RenderStones(int.Parse(textBoxNumStoneRows.Text), gameGrid, (Style)FindResource("playerStyle"));
             }
 
-            catch (Exception)   
+            catch (Exception)
             {
                 MessageBox.Show("Zadej pouze platné hodnoty!"); //Message that says: Only enter valid values!
             }
-        }
-
-        private bool OddChecker(bool oddRectangle, int x) //Return true only when last plate was false
-        {
-            oddRectangle = !oddRectangle;
-            if (x == damaGame.XSize - 1 && damaGame.XSize % 2 == 0) //only true when x(rows) are even
-                oddRectangle = !oddRectangle;
-
-            return oddRectangle;
         }
 
         private void Ellipse_MouseMove(object sender, MouseEventArgs e)
@@ -204,8 +97,8 @@ namespace WPF_Game_Checkers
                 damaGame.lastPlayerStonePosition = element;
                 DragDrop.DoDragDrop(element, element, DragDropEffects.Move);
 
-                attempsForMove++;
-                debugVariable.Content = attempsForMove.ToString();
+                damaGame.attempsForMove++;
+                debugVariable.Content = damaGame.attempsForMove.ToString();
             }
         }
 
@@ -214,10 +107,10 @@ namespace WPF_Game_Checkers
             //var newPosition = (UIElement)e.Source; //also working method, but cannot extract the "Name" from object
             var newPosition = e.OriginalSource as FrameworkElement; //get the element under mouse
 
-            if (validPosition[Grid.GetColumn(newPosition), Grid.GetRow(newPosition)] && (damaGame.lastPlayerStonePosition.Name.Substring(0, 5) == "black" ^ whiteMove) ) //Checks if the player moved the stone to valid color of checkerboard and if the player is allow to make move
+            if (damaGame.validPosition[Grid.GetColumn(newPosition), Grid.GetRow(newPosition)] && (damaGame.lastPlayerStonePosition.Name.Substring(0, 5) == "black" ^ damaGame.whiteMove) ) //Checks if the player moved the stone to valid color of checkerboard and if the player is allow to make move
             {
-                whiteMove = !whiteMove;
-                if (whiteMove)
+                damaGame.whiteMove = !damaGame.whiteMove;
+                if (damaGame.whiteMove)
                 {
                     setPlayerLabelColor(player1Label, true);
                     setPlayerLabelColor(player2Label, false);
@@ -228,19 +121,19 @@ namespace WPF_Game_Checkers
                     setPlayerLabelColor(player2Label, true);
                 }
 
-                if (playersLocation[Grid.GetRow(newPosition), Grid.GetColumn(newPosition)] == null) //check if there is no other player's stone (the destination space is empty)
+                if (damaGame.playersLocation[Grid.GetRow(newPosition), Grid.GetColumn(newPosition)] == null) //check if there is no other player's stone (the destination space is empty)
                 {
-                    if (MoveOneWayOnly(damaGame.lastPlayerStonePosition, newPosition) || freeMovement) //check if player is moving in right direction
+                    if (damaGame.MoveOneWayOnly(damaGame.lastPlayerStonePosition, newPosition, gameGrid) || damaGame.freeMovement) //check if player is moving in right direction
                     {
-                        playersLocation[Grid.GetRow(damaGame.lastPlayerStonePosition), Grid.GetColumn(damaGame.lastPlayerStonePosition)] = null;
+                        damaGame.playersLocation[Grid.GetRow(damaGame.lastPlayerStonePosition), Grid.GetColumn(damaGame.lastPlayerStonePosition)] = null;
                         Grid.SetColumn(damaGame.playerStone, Grid.GetColumn(newPosition));
                         Grid.SetRow(damaGame.playerStone, Grid.GetRow(newPosition));
 
-                        playersLocation[Grid.GetRow(newPosition), Grid.GetColumn(newPosition)] = (Ellipse)damaGame.playerStone;
-                        moves++;
-                        debugVariable2.Content = moves.ToString();
+                        damaGame.playersLocation[Grid.GetRow(newPosition), Grid.GetColumn(newPosition)] = (Ellipse)damaGame.playerStone;
+                        damaGame.moves++;
+                        debugVariable2.Content = damaGame.moves.ToString();
 
-                        var playersStatus = CheckAdversaryAlive();
+                        var playersStatus = damaGame.CheckAdversaryAlive();
                         if (!playersStatus.Item1)
                             MessageBox.Show("Hra skončila! Vyhrál hráč s " + (playersStatus.Item2 ? "Bílými figurkami." : "Černými figurkami.")); //Player with white/black figures won.
                     }
@@ -255,86 +148,10 @@ namespace WPF_Game_Checkers
                 label.Background = null;
         }
 
-        private bool MoveOneWayOnly(FrameworkElement lastPostion, FrameworkElement currentPosition)
-        {
-            int yLast = 0, yNow = 0;
-
-            yLast = Grid.GetRow(lastPostion);
-            yNow = Grid.GetRow(currentPosition);
-
-            if (lastPostion.Name.Substring(0, 5) == "white")
-            {
-                if ((yNow - yLast) == 1)
-                    return true;
-
-                else if ((yNow - yLast) == 2)
-                    return CheckKill(lastPostion, currentPosition);
-            }
-
-            else
-            {
-                if ((yNow - yLast) == -1)
-                    return true;
-
-                else if ((yNow - yLast) == -2)
-                    return CheckKill(lastPostion, currentPosition);
-            }
-
-            return false;
-        }
-
-        private bool CheckKill(FrameworkElement lastPosition, FrameworkElement currentPosition)
-        {
-            int xVictim = 0, yVictim = 0;
-
-            yVictim = (Grid.GetRow(lastPosition) + Grid.GetRow(currentPosition)) / 2;
-            xVictim = (Grid.GetColumn(lastPosition) + Grid.GetColumn(currentPosition)) / 2;
-
-            if (playersLocation[yVictim, xVictim] != null)
-            {
-                var victim = gameGrid.Children.Cast<FrameworkElement>().Last(value => Grid.GetRow(value) == yVictim && Grid.GetColumn(value) == xVictim); //Find UIElement by row/culumn values. ".Last" because Ellipses were created after rectangles(checkerboard)
-
-                if (victim.Name.Substring(0, 5) == lastPosition.Name.Substring(0, 5)) //Check if player's stones have different colors. if they are same return false
-                    return false;
-
-                gameGrid.Children.Remove(victim); //remove adversary
-
-                playersLocation[yVictim, xVictim] = null; //freeup space after elimination in gamesGrid
-                return true;
-            }
-            return false;
-        }
-
-        private Tuple<bool, bool> CheckAdversaryAlive()
-        {
-            bool white = false, black = false;
-
-            foreach (var item in playersLocation)
-            {
-                if (item != null)
-                {
-                    switch (item.Name.Substring(0, 5))
-                    {
-                        case "white":
-                            white = true;
-                            break;
-
-                        case "black":
-                            black = true;
-                            break;
-                    }
-                }
-
-                if (black && white)
-                    return Tuple.Create(true, false);
-            }
-            return Tuple.Create(false, white);
-        }
-
         private void ButtonTest_Click(object sender, RoutedEventArgs e)
         {
             string output = "";
-            foreach (Ellipse item in playersLocation)
+            foreach (Ellipse item in damaGame.playersLocation)
             {
                 if (item != null)
                 {
